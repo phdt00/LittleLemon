e3dba2012372eee058d91516f2eef65fe9d6dc96
phdt-ikea <peter.horstedt@inter.ikea.com>
2023-03-28 07:24:35 +0200

    Added DRF, views, and urls

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializer import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemsView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
